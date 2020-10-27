c7af6f12cf861fda6da8140c43f55bccb03cd6ec
GreyNade <greynadez@gmail.com>
2020-10-27 20:23:49 +0530

    3.1 Change

---
+++ app/src/main/java/com/esoxjem/movieguide/details/MovieDetailsActivity.java
@@ -0,0 +1,99 @@
+package com.esoxjem.movieguide.details;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+
+import com.esoxjem.movieguide.Constants;
+import com.esoxjem.movieguide.Movie;
+import com.esoxjem.movieguide.R;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+
+    private static InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        //TEST ADS
+
+        mInterstitialAd = new InterstitialAd(this);
+        //Main INT
+        // mInterstitialAd.setAdUnitId("ca-app-pub-4401386869031967/9145961501");
+
+        //TEST INT
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+
+        mInterstitialAd.setAdListener(new AdListener()
+                                      {
+                                          public void onAdLoaded() {
+
+                                              mInterstitialAd.loadAd(new AdRequest.Builder().build());
+                                              // Code to be executed when an ad finishes loading.
+                                          }
+
+                                          public void onAdOpened() {
+                                              // Code to be executed when the ad is displayed.
+                                          }
+
+                                          public void onAdClicked() {
+                                              // Code to be executed when the user clicks on an ad.
+                                          }
+
+                                          public void onAdLeftApplication() {
+                                              // Code to be executed when the user has left the app.
+                                          }
+
+                                          public void onAdClosed() {
+                                              mInterstitialAd.loadAd(new AdRequest.Builder().build());
+                                              // Code to be executed when the interstitial ad is closed.
+                                          }
+                                      }
+        );
+
+
+
+
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if (extras != null && extras.containsKey(Constants.MOVIE)) {
+                Movie movie = extras.getParcelable(Constants.MOVIE);
+                if (movie != null) {
+                    MovieDetailsFragment movieDetailsFragment = MovieDetailsFragment.getInstance(movie);
+                    getSupportFragmentManager().beginTransaction().add(R.id.movie_details_container, movieDetailsFragment).commit();
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
